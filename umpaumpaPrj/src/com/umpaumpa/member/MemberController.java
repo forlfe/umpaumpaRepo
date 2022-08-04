43b168b7c0002b17d39304392c4c89c44591048e
DESKTOP-HR2TCC7\joohwan1001 <jh20220713@github.com>
2022-08-04 11:07:04 +0900

    회원가입, 로그인

---
+++ umpaumpaPrj/src/com/umpaumpa/member/MemberController.java
@@ -0,0 +1,13 @@
+package com.umpaumpa.member;
+
+public class MemberController {
+	
+	public void login() {
+		System.out.println("로그인~~~");
+	}
+	
+	public void join() {
+		System.out.println("회원가입~~~");
+	}
+
+}
