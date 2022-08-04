6d8fbd37658f6c204834119b8217acf39fdcda48
DESKTOP-GQVTP40\haeyeion <haileyhy@naver.com>
2022-08-04 10:42:27 +0900

    초기셋팅

---
+++ umpaumpaPrj/src/com/umpaumpa/main/Main.java
@@ -0,0 +1,24 @@
+package com.umpaumpa.main;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("====== 환영합니다 수영 어플 음파음파입니다 ======");
+		while(true) {
+		System.out.println("1. 로그인");
+		System.out.println("2. 회원가입");
+		int input = sc.nextInt();
+		switch(input) {
+		case 1 : break;
+		case 2 : break;
+			}
+		}
+
+	}
+
+}
